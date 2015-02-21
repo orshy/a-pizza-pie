a348da410e94491c52ff4d7be9487153cbf8a0dd
Philip Orchard <PhilipOrchard@gmail.com>
2015-02-20 17:53:24 -0800

    Add html

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,36 @@ var Pizza = {
 
   },
   sizeRange: function() {
-    if (this.size <= 6 || this.size > 60)
+    if (this.size < 8 || this.size > 40)
     { return false; }
     return true;
   }
 };
+
+var empty = function() {
+  $("#input-size").val("");
+  $("#input-type").val("");
+  $("#results").hide();
+  $("#error").hide();
+}
+
+$(document).ready(function() {
+  $("#form-pizza").submit(function(event) {
+    var inputtedSize = parseInt($("#input-size").val());
+    var inputtedType = $("#input-type").val();
+    empty();
+    var pizza = Object.create(Pizza);
+    pizza.details(inputtedSize, inputtedType);
+    if(pizza.sizeRange()) {
+      var slice = pizza.slices();
+      $("#results h5").text("Your " + pizza.size + " in. " + pizza.type + " pizza will arrive in " + slice + " slices.");
+    $("#results").show();
+  } else {
+    $("#error h5").text("We do not make a " + pizza.size + " in. pizza");
+    $("#error").show();
+  }
+
+  event.preventDefault();
+  });
+
+});
