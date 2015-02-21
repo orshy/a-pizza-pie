0667a1bbc7742af2b9576fe11aaff84169d5c5e5
Philip Orchard <PhilipOrchard@gmail.com>
2015-02-20 16:36:50 -0800

    Second spec passing

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,13 @@ var Pizza = {
     this.size = size;
     this.type = type;
   },
+  slices: function() {
+    if (this.size <= 12) { return 4; }
+    else if (this.size <= 18) {return 6; }
+    else if (this.size <= 24) {return 8; }
+    else { return 12; }
+
+
+  }
 
 };
--- specs/specs.js
+++ specs/specs.js
@@ -1,14 +1,18 @@
 describe("Pizza", function() {
 
   describe("details", function() {
-    it("Will create object size and type", function() {
+    it("will create object size and type", function() {
       var salePie = Object.create(Pizza);
       salePie.details(18, "cheese");
       expect(salePie.size).to.equal(18);
     });
-
-
   });
 
-
+  describe("slices", function() {
+    it("will determine number of slices based on size", function() {
+        var salePie = Object.create(Pizza);
+        salePie.details(18, "cheese");
+        expect(salePie.slices()).to.equal(6);
+    });
+  });
 });
